a0cf727d03f78467146a79768486eb364bf450db
Larissagouveia <larissa.gouveia@sptech.school>
2022-11-30 22:06:41 -0300

    plotar gráficos

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Projeto-dash
+# Projeto Take One - REC - Pesquisa e Inovação
+<h2 align="center">São Paulo Tech School</h2>
+<h4 align="center" >
+  <span>Ezequiel Leandro – 03221063</span><br>
+<span>Henrique Medeiros – 03221029</span><br>
+<span>Matheus Tonini – 03221025</span><br>
+<span>Mauricio Uesso – 03221039</span><br>
+<span>Larissa Gouveia– 03221031</span><br>
+</h4>
+<h5>Aplicação cliente e Web para o monitoramento de componentes de hardware / sistemas operacionais, utilizando os conceitos do ITIL de Monitoramento de Serviços, Gestão de Incidentes e Gestão de Problemas.
+<br><br>
+Empresa: REC<br>
+Projeto: Take One | Monitoramento do Cinema<br>
+Segmento: Mercado AudioVisual<br>
+Meta: Monitorar o sistema operacional dos Totens / Controle Financeiro / Análises e Relatórios / Estoque e Inventário (Informações sobre Salas) / Controle de Funcionários
+</h5>
---
+++ site/src/models/usuarioModel.js
@@ -0,0 +1,51 @@
+var database = require("../database/config")
+
+function listar() {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
+    var instrucao = `
+        SELECT * FROM usuario;
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+function entrar(email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    var instrucao = `
+        SELECT  usuario.*, Filial.nomeFantasia FROM usuario join Filial on fkFilial = idFilial WHERE emailUsuario = '${email}' AND senhaUsuario = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+// Coloque os mesmos parâmetros aqui. Vá para a var instrucao
+function cadastrar(fkFilial, fkCredencial, nomeUsuario, email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():",fkFilial, fkCredencial, nomeUsuario, email, senha);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucao = `
+        INSERT INTO Usuario (fkFilial, fkLicenca, nomeUsuario, emailUsuario, senhaUsuario) VALUES (${fkFilial}, ${fkCredencial}, '${nomeUsuario}', '${email}', '${senha}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+function alertas(componente,metrica,empresa,criticidade) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():",componente,metrica,empresa,criticidade);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucao = `
+        INSERT INTO Chamados (Empresa, Componente, Metrica, Criticidade, DataHora) VALUES ('${empresa}', '${componente}', '${metrica}', '${criticidade}', GETDATE());
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+module.exports = {
+    entrar,
+    cadastrar,
+    listar,
+    alertas
+};
